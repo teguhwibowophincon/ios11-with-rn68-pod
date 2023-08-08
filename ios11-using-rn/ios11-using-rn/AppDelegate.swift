4a24bd361baf6829b19d7aab532ff076959bf130
teguhwibowophincon <teguh.wibowo@phincon.com>
2023-08-08 17:45:10 +0700

    Commit all

---
+++ ios11-using-rn/ios11-using-rn/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ios11-using-rn
+//
+//  Created by PT Phincon on 08/08/23.
+//
+
+import UIKit
+import rn68
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ ios11-using-rn/ios11-using-rn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios11-using-rn
+//
+//  Created by PT Phincon on 08/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
